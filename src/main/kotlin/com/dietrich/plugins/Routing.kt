0a20d296d3b0ca5f9f68f2f37cc86e9c88b55c7f
rafael <rafael.ldietrich@gmail.com>
2022-09-11 17:42:04 -0300

    smelly code

--- src/main/kotlin/com/dietrich/plugins/Routing.kt
+++ src/main/kotlin/com/dietrich/plugins/Routing.kt
@@ -58,3 +58,8 @@ private fun fetchUser(id: Long): User? {
 
     return null
 }
+
+private fun findActive(active: Boolean = true) {
+    val enum = active
+    users.filterValues { it.active == enum }
+}
